715210acf55b22c9b41e4cf7dedcb0b9dd6f4384
Keivan-sf <keyvan0082@gmail.com>
2023-11-03 19:00:41 +0330

    chore: read port from `.env`

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Elysia } from "elysia";
 import * as DB from "./database";
 import * as Books from "./components/Books/books.controller";
+const PORT = process.env.PORT ? +process.env.PORT : 3002;
 
 await DB.initialize();
 const app = new Elysia();
 Books.route(app);
 
-app.listen(3002);
+app.listen(PORT);
 console.log(`App is running on ${app.server?.hostname}:${app.server?.port}`);
