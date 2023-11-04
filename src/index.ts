d0307af825648bd6b503d48cd1edebf55b9ff180
Keivan-sf <keyvan0082@gmail.com>
2023-11-04 09:44:05 +0330

    feat: add request error handler

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,18 @@ const PORT = process.env.PORT ? +process.env.PORT : 3002;
 
 await DB.initialize();
 const app = new Elysia();
-Books.route(app);
-app.onError(({ error }) => {
-  console.dir(error);
-  return "internal error";
+
+app.onError(({ error, code, set }) => {
+  const known_errors = ["NOT_FOUND", "VALIDATION", "PARSE"];
+  if (known_errors.includes(code.toUpperCase())) {
+    return { success: false, error: error.message };
+  }
+  console.dir(error, code);
+  set.status = 500;
+  return { success: false, error: "An unexpected error occurred" };
 });
+
+Books.route(app);
 app.listen(PORT);
 
 console.log(`App is running on ${app.server?.hostname}:${app.server?.port}`);
